67034e5bbd32d9132f8c8c1564c9e3be4f02abf3
Nusrath Khan <nusrath501khan@gmail.com>
2015-10-20 21:47:11 +0200

    ci stuffs

--- test/index.js
+++ test/index.js
@@ -3,12 +3,24 @@ var should = require('chai').should(),
 
 describe('#get_config', function() {
   it('gets the current status of the deluge client', function() {
-    deluge.get_config(function(data){ var ret = typeOf data; ret.should.equal("object")});
+    deluge.get_config(
+    	function(data){
+    		var ret = typeof data; 
+    		console.log(ret); 
+    		ret.should.equal("object")
+    	}
+    );
   });
 });
 
 describe('#get_status', function() {
   it('gets the current config of the deluge client', function() {
-    deluge.get_status(function(data){ var ret = typeOf data; ret.should.equal("object")});
+    deluge.get_status(
+    	function(data){ 
+    		var ret = typeof data; 
+    		console.log(ret); 
+    		ret.should.equal("object")
+    	}
+    );
   });
 });
