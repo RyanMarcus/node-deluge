ec55f3e1bb3cc6da44773c6b49f0a7ed3b067b06
Ryan Marcus <ryan@rmarcus.info>
2016-01-16 18:33:52 -0500

    updated test

--- test/index.js
+++ test/index.js
@@ -2,23 +2,27 @@ var should = require('chai').should(),
     deluge = require('../index')('sd-80083.dedibox.fr', 'deluge', null);
 
 describe('#get_config', function() {
-  it('gets the current status of the deluge client', function() {
-    deluge.get_config(
-    	function(data){
-    		should.exist(data);
-  			data.should.be.an('object');
-    	}
-    );
-  });
+	it('gets the current status of the deluge client', function() {
+		deluge.auth(function (res) {
+			deluge.get_config(
+    				function(data){
+    					should.exist(data);
+  					data.should.be.an('object');
+    				}
+			);
+		});
+	});
 });
 
 describe('#get_status', function() {
-  it('gets the current config of the deluge client', function() {
-    deluge.get_status(
-    	function(data){
-    		should.exist(data);
-  			data.should.be.an('object');
-    	}
-    );
-  });
+	it('gets the current config of the deluge client', function() {
+		deluge.auth(function (res) {
+			deluge.get_status(
+    				function(data){
+    					should.exist(data);
+  					data.should.be.an('object');
+    				});
+		});
+		
+	});
 });
